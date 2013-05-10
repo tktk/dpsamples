2338a37d9ee102d7e1b0b5418780bb98a8c79cfc
tekuto <g.tekuto@gmail.com>
2013-05-10 21:23:02 +0900

    displayについて、 ・dp::DisplayManager、dp::DisplayManagerInfoの生成処理を追加 ・終了待機処理を追加

--- sample/display/src/main.cpp
+++ sample/display/src/main.cpp
@@ -1,10 +1,22 @@
 ﻿#include "dp/dp.h"
+#include "dp/display/displaymanager.h"
+#include "dp/display/displaymanagerinfo.h"
+
+#include <cstdio>
 
 int dpMain(
     dp::Args &
 )
 {
+    dp::DisplayManagerInfo  displayManagerInfo;
+
     //TODO
+
+    dp::DisplayManager  displayManager( displayManagerInfo );
+
+    std::printf( "Press ENTER to quit\n" );
+
+    std::getchar();
 
     return 0;
 }
