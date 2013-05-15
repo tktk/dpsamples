13fa942135c77f4c20d11fe19cd504c74e52459f
tekuto <g.tekuto@gmail.com>
2013-05-15 22:46:13 +0900

    displayについて、 ・イベントハンドラ設定処理を追加 ・dp_displayをリンクするように修正

--- sample/display/src/main.cpp
+++ sample/display/src/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include "dp/dp.h"
 #include "dp/display/displaymanager.h"
 #include "dp/display/displaymanagerinfo.h"
+#include "dp/display/displaykey.h"
 
 #include <cstdio>
 
@@ -8,9 +9,31 @@ int dpMain(
     dp::Args &
 )
 {
-    dp::DisplayManagerInfo  displayManagerInfo;
-
     //TODO
+
+    dp::DisplayManagerInfo  displayManagerInfo;
+    displayManagerInfo.setConnectEventHandler(
+        [](
+            dp::DisplayManager &        _manager
+            , const dp::DisplayKey &    _KEY
+        )
+        {
+            //TODO
+
+            std::printf( "display connected\n" );
+        }
+    );
+    displayManagerInfo.setDisconnectEventHandler(
+        [](
+            dp::DisplayManager &        _manager
+            , const dp::DisplayKey &    _KEY
+        )
+        {
+            //TODO
+
+            std::printf( "display disconnected\n" );
+        }
+    );
 
     dp::DisplayManager  displayManager( displayManagerInfo );
 
