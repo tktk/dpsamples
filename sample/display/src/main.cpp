3170e233c7368f96715a7a7c27eee59b3ed944f4
tekuto <g.tekuto@gmail.com>
2013-05-22 14:54:21 +0900

    displayのディスプレイ接続イベント発生時にディスプレイの情報取得処理を追加

--- sample/display/src/main.cpp
+++ sample/display/src/main.cpp
@@ -2,6 +2,8 @@
 #include "dp/display/displaymanager.h"
 #include "dp/display/displaymanagerinfo.h"
 #include "dp/display/displaykey.h"
+#include "dp/display/display.h"
+#include "dp/display/displaymode.h"
 
 #include <cstdio>
 
@@ -9,8 +11,6 @@ int dpMain(
     dp::Args &
 )
 {
-    //TODO
-
     dp::DisplayManagerInfo  displayManagerInfo;
     displayManagerInfo.setConnectEventHandler(
         [](
@@ -18,9 +18,33 @@ int dpMain(
             , const dp::DisplayKey &    _KEY
         )
         {
-            //TODO
+            const auto  DISPLAY = _manager.getDisplay(
+                _KEY
+            );
+
+            const auto  DISPLAY_MODE = _manager.getDisplayMode(
+                DISPLAY.getModeKey()
+            );
 
             std::printf( "display connected\n" );
+            std::printf(
+                "  position : %dx%d\n"
+                , DISPLAY.getX()
+                , DISPLAY.getY()
+            );
+            std::printf(
+                "  size : %dx%d\n"
+                , DISPLAY.getWidth()
+                , DISPLAY.getHeight()
+            );
+            std::printf(
+                "  mode :\n"
+            );
+            std::printf(
+                "    size : %dx%d\n"
+                , DISPLAY_MODE.getWidth()
+                , DISPLAY_MODE.getHeight()
+            );
         }
     );
     displayManagerInfo.setDisconnectEventHandler(
@@ -29,8 +53,6 @@ int dpMain(
             , const dp::DisplayKey &    _KEY
         )
         {
-            //TODO
-
             std::printf( "display disconnected\n" );
         }
     );
