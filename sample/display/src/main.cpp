78f7745041087ccedc9624cf061ba5f02464c37a
tekuto <g.tekuto@gmail.com>
2013-05-23 14:03:27 +0900

    displayにdp::DisplayManagerなどの仕様変更に伴う修正を適用

--- sample/display/src/main.cpp
+++ sample/display/src/main.cpp
@@ -7,6 +7,39 @@
 
 #include <cstdio>
 
+void printDisplayInfo(
+    dp::DisplayManager &        _manager
+    , const dp::DisplayKey &    _KEY
+)
+{
+    const auto  DISPLAY = _manager.getDisplay(
+        _KEY
+    );
+
+    const auto  DISPLAY_MODE = _manager.getDisplayMode(
+        DISPLAY.getModeKey()
+    );
+
+    std::printf(
+        "  position : %dx%d\n"
+        , DISPLAY.getX()
+        , DISPLAY.getY()
+    );
+    std::printf(
+        "  size : %dx%d\n"
+        , DISPLAY.getWidth()
+        , DISPLAY.getHeight()
+    );
+    std::printf(
+        "  mode :\n"
+    );
+    std::printf(
+        "    size : %dx%d\n"
+        , DISPLAY_MODE.getWidth()
+        , DISPLAY_MODE.getHeight()
+    );
+}
+
 int dpMain(
     dp::Args &
 )
@@ -16,44 +49,19 @@ int dpMain(
         [](
             dp::DisplayManager &        _manager
             , const dp::DisplayKey &    _KEY
+            , bool                      _connected
         )
         {
-            const auto  DISPLAY = _manager.getDisplay(
-                _KEY
-            );
+            if( _connected ) {
+                std::printf( "display connected\n" );
 
-            const auto  DISPLAY_MODE = _manager.getDisplayMode(
-                DISPLAY.getModeKey()
-            );
-
-            std::printf( "display connected\n" );
-            std::printf(
-                "  position : %dx%d\n"
-                , DISPLAY.getX()
-                , DISPLAY.getY()
-            );
-            std::printf(
-                "  size : %dx%d\n"
-                , DISPLAY.getWidth()
-                , DISPLAY.getHeight()
-            );
-            std::printf(
-                "  mode :\n"
-            );
-            std::printf(
-                "    size : %dx%d\n"
-                , DISPLAY_MODE.getWidth()
-                , DISPLAY_MODE.getHeight()
-            );
-        }
-    );
-    displayManagerInfo.setDisconnectEventHandler(
-        [](
-            dp::DisplayManager &        _manager
-            , const dp::DisplayKey &    _KEY
-        )
-        {
-            std::printf( "display disconnected\n" );
+                printDisplayInfo(
+                    _manager
+                    , _KEY
+                );
+            } else {
+                std::printf( "display disconnected\n" );
+            }
         }
     );
 
